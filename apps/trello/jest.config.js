c3cb3fa61789f3fa2a82a0657fe3dcf133de3235
Vivek <vivek_subramoniapillai@trimble.com>
2020-02-04 15:47:47 +0530

    removed app

--- apps/trello/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'trello',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/trello',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
-};
